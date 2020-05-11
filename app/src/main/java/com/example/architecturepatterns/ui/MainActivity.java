40e9fec17a0c75f837f4bca31cb1fd3b84d60f2b
Ibrahim Shaltout <40940509+ibrahinShaltout@users.noreply.github.com>
2020-05-11 12:28:26 +0200

    MVVM - Model View View Model - DataBinding version 1

--- app/src/main/java/com/example/architecturepatterns/ui/MainActivity.java
+++ app/src/main/java/com/example/architecturepatterns/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.architecturepatterns.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -8,37 +9,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.example.architecturepatterns.R;
+import com.example.architecturepatterns.databinding.ActivityMainBinding;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.textView)
-    TextView movieNameTV;
-    @BindView(R.id.bottom)
-    Button getMovieButton;
+public class MainActivity extends AppCompatActivity {
+
     MovieViewModle movieViewModle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        getMovieButton.setOnClickListener(this);
-
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.bottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movieViewModle.getMovieName();
+            }
+        });
         movieViewModle = ViewModelProviders.of(this).get(MovieViewModle.class);
         movieViewModle.movieNameMutableLiveData.observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
-                movieNameTV.setText(s);
+                binding.textView.setText(s);
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.bottom) {
-            movieViewModle.getMovieName();
-        }
     }
 }
--- app/src/main/java/com/example/architecturepatterns/ui/MovieView.java
+++
@@ -1,5 +0,0 @@
-package com.example.architecturepatterns.ui;
-
-public interface MovieView {
-    void onGetMovieName(String movieName);
-}
